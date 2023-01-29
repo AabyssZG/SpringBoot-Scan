c9dbe1657c162c1796cb310ecac7906761f7a3bf
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-01-29 15:48:39 +0800

    Add files via upload

---
+++ SpringBoot-Scan.py
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/env python
+# coding=utf-8
+  ################
+ #   AabyssZG   #
+################
+
+from inc import output, console, run
+import re, binascii, argparse, sys, time
+
+def get_parser():
+    parser = argparse.ArgumentParser(usage='python3 SpringBoot-Scan.py',description='SpringBoot-Scan: 针对SpringBoot的开源渗透框架',)
+    p = parser.add_argument_group('FileReverse-Tools 的参数')
+    p.add_argument("-u", "--url", type=str, help="对单一URL进行信息泄露扫描")
+    p.add_argument("-f", "--file", type=str, help="读取目标TXT进行信息泄露扫描")
+    p.add_argument("-v", "--vul", type=str, help="对单一URL进行漏洞利用")
+    p.add_argument("-d", "--dump", type=str, help="扫描并下载SpringBoot敏感文件（可提取敏感信息）")
+    args = parser.parse_args()
+    return args
+
+def main():
+    output.logo()
+    args = get_parser()
+    console.SpringBoot_Scan_console(args)
+
+if __name__ == '__main__':
+    main()

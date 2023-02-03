cf7e580d1b9b5e82907cd86d55d1c4787cb52493
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-02-03 12:53:43 +0800

    Update SpringBoot-Scan.py

--- SpringBoot-Scan.py
+++ SpringBoot-Scan.py
@@ -1,10 +1,10 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # coding=utf-8
   ################
  #   AabyssZG   #
 ################
 
-from inc import output, console, run
+from inc import output, console, run ,proxycheck
 import re, binascii, argparse, sys, time
 
 def get_parser():
@@ -14,13 +14,15 @@ def get_parser():
     p.add_argument("-f", "--file", type=str, help="读取目标TXT进行信息泄露扫描")
     p.add_argument("-v", "--vul", type=str, help="对单一URL进行漏洞利用")
     p.add_argument("-d", "--dump", type=str, help="扫描并下载SpringBoot敏感文件（可提取敏感信息）")
+    p.add_argument("-p", "--proxy", type=str, default='', help="使用HTTP代理")
     args = parser.parse_args()
     return args
 
 def main():
     output.logo()
     args = get_parser()
-    console.SpringBoot_Scan_console(args)
+    proxycheck.SpringBoot_Scan_Proxy(args)
+    #console.SpringBoot_Scan_console(args)
 
 if __name__ == '__main__':
     main()

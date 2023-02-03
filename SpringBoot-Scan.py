9d8e13c404896f77479670efc0c9b2a74b6a94ab
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-02-03 13:42:38 +0800

    Update SpringBoot-Scan.py

--- SpringBoot-Scan.py
+++ SpringBoot-Scan.py
@@ -9,7 +9,7 @@ import re, binascii, argparse, sys, time
 
 def get_parser():
     parser = argparse.ArgumentParser(usage='python3 SpringBoot-Scan.py',description='SpringBoot-Scan: 针对SpringBoot的开源渗透框架',)
-    p = parser.add_argument_group('FileReverse-Tools 的参数')
+    p = parser.add_argument_group('SpringBoot-Scan 的参数')
     p.add_argument("-u", "--url", type=str, help="对单一URL进行信息泄露扫描")
     p.add_argument("-f", "--file", type=str, help="读取目标TXT进行信息泄露扫描")
     p.add_argument("-v", "--vul", type=str, help="对单一URL进行漏洞利用")

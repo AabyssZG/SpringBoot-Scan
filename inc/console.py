9e25188c57194c21f30d3137c72472eea1027798
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-11-19 20:00:38 +0800

    Update console.py

--- inc/console.py
+++ inc/console.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-from inc import output,run,vul
+from inc import output,run,vul,springcheck
 import sys
 
 # 控制台-参数处理和程序调用
 def SpringBoot_Scan_console(args,proxies):
     if args.url:
-        run.url(args.url,proxies)
+        urlnew = springcheck.check(args.url,proxies)
+        run.url(urlnew,proxies)
     if args.file:
         run.file(args.file,proxies)
     if args.vul:
-        vul.vul(args.vul,proxies)
+        urlnew = springcheck.check(args.vul,proxies)
+        vul.vul(urlnew,proxies)
     if args.dump:
-        run.dump(args.dump,proxies)
+        urlnew = springcheck.check(args.dump,proxies)
+        run.dump(urlnew,proxies)
     else:
         output.usage()
         sys.exit()

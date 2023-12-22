12f35dbd8886988ff4bcb9bf2efe820b21c81f26
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-12-22 10:13:51 +0800

    Update SpringBoot-Scan.py

--- SpringBoot-Scan.py
+++ SpringBoot-Scan.py
@@ -11,11 +11,12 @@ def get_parser():
     parser = argparse.ArgumentParser(usage='python3 SpringBoot-Scan.py',description='SpringBoot-Scan: 针对SpringBoot的开源渗透框架',)
     p = parser.add_argument_group('SpringBoot-Scan 的参数')
     p.add_argument("-u", "--url", type=str, help="对单一URL进行信息泄露扫描")
-    p.add_argument("-f", "--file", type=str, help="读取目标TXT进行信息泄露扫描")
+    p.add_argument("-uf", "--urlfile", type=str, help="读取目标TXT进行信息泄露扫描")
     p.add_argument("-v", "--vul", type=str, help="对单一URL进行漏洞利用")
     p.add_argument("-d", "--dump", type=str, help="扫描并下载SpringBoot敏感文件（可提取敏感信息）")
     p.add_argument("-p", "--proxy", type=str, default='', help="使用HTTP代理")
     p.add_argument("-z", "--zoomeye", type=str, default='', help="使用ZoomEye导出Spring框架资产")
+    p.add_argument("-f", "--fofa", type=str, default='', help="使用Fofa导出Spring框架资产")
     args = parser.parse_args()
     return args
 

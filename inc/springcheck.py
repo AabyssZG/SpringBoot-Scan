2ae68fab2825fa42954a7ced73b274f88bcd50e7
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-12-31 12:54:35 +0800

    Update springcheck.py

--- inc/springcheck.py
+++ inc/springcheck.py
@@ -1,20 +1,22 @@
-﻿#!/usr/bin/env python
+#!/usr/bin/env python
 # coding=utf-8
+  ################
+ #   AabyssZG   #
+################
 
 from inc import output,run,vul,console
-import requests, sys, hashlib
+import requests, sys, hashlib, json
 from termcolor import cprint
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
-def Spring_Check(url,proxies):
+def Spring_Check(url,proxies,header_new):
     cprint("[.] 正在进行Spring的指纹识别","cyan")
     Spring_hash = "0488faca4c19046b94d07c3ee83cf9d6"
     Paths = ["favicon.ico", "AabyssZG666"]
-
     for path in Paths:
         test_url = str(url) + path
-        r = requests.get(test_url, timeout=10, verify=False, proxies=proxies)
+        r = requests.get(test_url, timeout=10, verify=False, headers=header_new, proxies=proxies)
         try:
             content_type = r.headers.get("Content-Type", "")
             if "image" in content_type or "octet-stream" in content_type:
@@ -36,22 +38,23 @@ def Spring_Check(url,proxies):
             f2.write(str(e) + '\n')
             f2.close()
 
-def check(url,proxies):
+def check(url,proxies,header_new):
+    header_new = json.loads(header_new)
     if ('://' not in url):
         url = str("http://") + str(url)
     if str(url[-1]) != "/":
         url = url + "/"
     try:
         requests.packages.urllib3.disable_warnings()
-        r = requests.get(url, timeout=6, verify=False, proxies=proxies)  # 设置超时6秒
+        r = requests.get(url, timeout=6, verify=False, headers=header_new, proxies=proxies)  # 设置超时6秒
         if r.status_code == 503:
             sys.exit()
         else:
-            Spring_Check(url,proxies)
+            Spring_Check(url,proxies,header_new)
             return url
     except KeyboardInterrupt:
         print("Ctrl + C 手动终止了进程")
         sys.exit()
-    except:
+    except Exception as e:
         cprint("[-] URL为 " + url + " 的目标积极拒绝请求，予以跳过！", "magenta")
         sys.exit()

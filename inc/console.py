954f37607f6204e6c28494045ea8098d286a147c
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2023-12-31 12:53:09 +0800

    Update console.py

--- inc/console.py
+++ inc/console.py
@@ -8,20 +8,20 @@ from inc import output,run,vul,springcheck,zoom,fofa,poc,hunter
 import sys
 
 # 控制台-参数处理和程序调用
-def SpringBoot_Scan_console(args,proxies):
+def SpringBoot_Scan_console(args, proxies, header_new):
     if args.url:
-        urlnew = springcheck.check(args.url,proxies)
-        run.url(urlnew,proxies)
+        urlnew = springcheck.check(args.url, proxies, header_new)
+        run.url(urlnew, proxies, header_new)
     if args.urlfile:
-        run.file(args.urlfile,proxies)
+        run.file(args.urlfile,proxies, header_new)
     if args.vul:
-        urlnew = springcheck.check(args.vul,proxies)
-        vul.vul(urlnew,proxies)
+        urlnew = springcheck.check(args.vul, proxies, header_new)
+        vul.vul(urlnew, proxies, header_new)
     if args.vulfile:
-        poc.poc(args.vulfile,proxies)
+        poc.poc(args.vulfile, proxies)
     if args.dump:
-        urlnew = springcheck.check(args.dump,proxies)
-        run.dump(urlnew,proxies)
+        urlnew = springcheck.check(args.dump, proxies, header_new)
+        run.dump(urlnew, proxies, header_new)
     if args.zoomeye:
         zoom.ZoomDowload(args.zoomeye,proxies)
     if args.fofa:

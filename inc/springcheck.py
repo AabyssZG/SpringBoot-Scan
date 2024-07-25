0d1747bb661ef8c0fe04425d7271891f0c61d306
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2024-07-25 10:47:35 +0800

    Update springcheck.py

--- inc/springcheck.py
+++ inc/springcheck.py
@@ -14,21 +14,23 @@ def Spring_Check(url,proxies,header_new):
     cprint("[.] 正在进行Spring的指纹识别","cyan")
     Spring_hash = "0488faca4c19046b94d07c3ee83cf9d6"
     Paths = ["favicon.ico", "AabyssZG666"]
+    check_status = 0
     for path in Paths:
         test_url = str(url) + path
         r = requests.get(test_url, timeout=10, verify=False, headers=header_new, proxies=proxies)
         try:
             content_type = r.headers.get("Content-Type", "")
-            if "image" in content_type or "octet-stream" in content_type:
+            if r.text and ('timestamp' in r.text):
+                cprint("[+] 站点报错内容符合Spring特征，识别成功","red")
+                check_status = 1
+            elif "image" in content_type or "octet-stream" in content_type:
                 favicon_hash = hashlib.md5(r.content).hexdigest()
                 if favicon_hash == Spring_hash:
                     cprint("[+] 站点Favicon是Spring图标，识别成功","red")
-                    break
-            elif r.text and ('timestamp' in r.text):
-                cprint("[+] 站点报错内容符合Spring特征，识别成功","red")
-                break
-            else:
+                    check_status = 1
+            while check_status == 0:
                 cprint("[-] 站点指纹不符合Spring特征，可能不是Spring框架","yellow")
+                check_status = 2
         except KeyboardInterrupt:
             print("Ctrl + C 手动终止了进程")
             sys.exit()

7f9b6c448b707b7fbb21b96e1321be53688b00fb
曾哥 <54609266+AabyssZG@users.noreply.github.com>
2025-03-10 23:24:39 +0800

    Update output.py

--- inc/output.py
+++ inc/output.py
@@ -18,11 +18,11 @@ def logo():
  \$$    $$| $$    $$| $$      | $$| $$  | $$ \$$    $$| $$    $$ \$$    $$ \$$    $$  \$$  $$
   \$$$$$$ | $$$$$$$  \$$       \$$ \$$   \$$ _\$$$$$$$ \$$$$$$$   \$$$$$$   \$$$$$$    \$$$$ 
           | $$                              |  \__| $$                                       
-          | $$                               \$$    $$   [+] V2.53-2024年 龙年新春贺岁版     
+          | $$                               \$$    $$   [+] V2.6-2025年 蛇年新春贺岁版     
            \$$                                \$$$$$$    [+] 感谢一路上支持和关注我们的师傅  
             ______                                                                           
            /      \                                  +-------------------------------------+ 
-          |  $$$$$$\  _______  ______   _______      + Version: 2.56                       + 
+          |  $$$$$$\  _______  ______   _______      + Version: 2.6                        + 
           | $$___\$$ /       \|      \ |       \     + Author: 曾哥(@AabyssZG)             + 
            \$$    \ |  $$$$$$$ \$$$$$$\| $$$$$$$\    + Whoami: https://github.com/AabyssZG + 
            _\$$$$$$\| $$      /      $$| $$  | $$    +-------------------------------------+ 
@@ -36,23 +36,16 @@ def usage():
     print('''
 用法:
         对单一URL进行信息泄露扫描:         python3 SpringBoot-Scan.py -u http://example.com/
-        读取目标TXT进行批量信息泄露扫描:    python3 SpringBoot-Scan.py -uf url.txt
+        读取目标TXT进行批量信息泄露扫描:   python3 SpringBoot-Scan.py -uf url.txt
         对单一URL进行漏洞扫描:             python3 SpringBoot-Scan.py -v http://example.com/
         读取目标TXT进行批量漏洞扫描：      python3 SpringBoot-Scan.py -vf url.txt
         扫描并下载SpringBoot敏感文件:      python3 SpringBoot-Scan.py -d http://example.com/
+        读取目标TXT进行批量敏感文件扫描:   python3 SpringBoot-Scan.py -df url.txt
         使用HTTP代理并自动进行连通性测试:    python3 SpringBoot-Scan.py -p <代理IP:端口>
         从TXT文件中导入自定义HTTP头部:       python3 SpringBoot-Scan.py -t header.txt
         通过ZoomEye密钥进行API下载数据:      python3 SpringBoot-Scan.py -z <ZoomEye的API-KEY>
         通过Fofa密钥进行API下载数据:         python3 SpringBoot-Scan.py -f <Fofa的API-KEY>
         通过Hunter密钥进行API下载数据:       python3 SpringBoot-Scan.py -y <Hunter的API-KEY>
-
-██╗  ██╗ █████╗ ██████╗ ██████╗ ██╗   ██╗    ███╗   ██╗███████╗██╗    ██╗██╗   ██╗███████╗ █████╗ ██████╗ 
-██║  ██║██╔══██╗██╔══██╗██╔══██╗╚██╗ ██╔╝    ████╗  ██║██╔════╝██║    ██║╚██╗ ██╔╝██╔════╝██╔══██╗██╔══██╗
-███████║███████║██████╔╝██████╔╝ ╚████╔╝     ██╔██╗ ██║█████╗  ██║ █╗ ██║ ╚████╔╝ █████╗  ███████║██████╔╝
-██╔══██║██╔══██║██╔═══╝ ██╔═══╝   ╚██╔╝      ██║╚██╗██║██╔══╝  ██║███╗██║  ╚██╔╝  ██╔══╝  ██╔══██║██╔══██╗
-██║  ██║██║  ██║██║     ██║        ██║       ██║ ╚████║███████╗╚███╔███╔╝   ██║   ███████╗██║  ██║██║  ██║
-╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝        ╚═╝       ╚═╝  ╚═══╝╚══════╝ ╚══╝╚══╝    ╚═╝   ╚══════╝╚═╝  ╚═╝╚═╝  ╚═╝
-                         [+] SpringBoot-Scan V2.52-2024年 龙年新春贺岁版
 
 免责声明：
         1.如果您下载、安装、使用、修改本工具及相关代码，即表明您信任本工具
@@ -70,6 +63,7 @@ def usage():
         -v  --vul       对单一URL进行漏洞利用
         -vf  --vulfile  读取目标TXT进行批量漏洞扫描
         -d  --dump      扫描并下载SpringBoot敏感文件（可提取敏感信息）
+        -df  --dumpfile 读取目标TXT进行批量敏感文件扫描（可提取敏感信息）
         -p  --proxy     使用HTTP进行代理（默认连通性测试www.baidu.com）
         -z  --zoomeye   通过对接ZoomEye的API批量下载Spring的资产测绘数据
         -f  --fofa      通过对接Fofa的API批量下载Spring的资产测绘数据
